415da83e05592943823ba79d63a32b8050491953
SalgadoAndres <andresf_salgadoc@soy.sena.edu.co>
2024-03-13 10:42:42 -0500

    cambios del proyecto

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,15 +2,18 @@ import { HomeComponent } from "./components/home/home";
 import { CalificacionesComponent } from "./components/calificaciones/calificaciones"; 
 import { LoginComponent } from "./components/login/login";
 import { VerPerfilComponent } from "./components/verPerfil/verperfil";
-
+import { ProductosComponent } from "./components/productos/productos";
+import { Producto_VistaComponent } from "./components/producto_vista/producto_vista";
 
 
 export const appRoutes: any = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'productos', pathMatch: 'full' },
   { path: "login", component: LoginComponent },
   { path: "home", component: HomeComponent },
   { path: "calificaciones", component: CalificacionesComponent },
   { path: "verperfil", component: VerPerfilComponent },
+  { path: "productos", component: ProductosComponent },
+  { path: "producto_vista", component: Producto_VistaComponent },
 
 ];
 
@@ -19,5 +22,7 @@ export const appComponents: any = [
   CalificacionesComponent,
   LoginComponent,
   VerPerfilComponent,
+  ProductosComponent,
+  Producto_VistaComponent,
 
 ];
